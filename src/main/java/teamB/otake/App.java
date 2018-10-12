b99f356555e7b115f4fdeb6f4ab3142ca58956cd
rubenhouri <ruben.houri@etu.unice.fr>
2018-10-13 00:46:34 +0200

    Ajout d'un des qui prend en argument des objets face et qui en renvoie une de façon aléatoire.

--- src/main/java/teamB/otake/App.java
+++ src/main/java/teamB/otake/App.java
@@ -1,11 +1,22 @@
-package teamB.otake;
+package main.java.teamB.otake;
 
-/**
- * Hello world!
- *
- */
+
+import java.util.ArrayList;
+
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ArrayList<face> faces = new ArrayList<>();
+        face face1 = new face(1);
+        face face2 = new face(2);
+        face face3 = new face(3);
+        faces.add(face1);faces.add(face2);faces.add(face3);
+        dice des = new dice(faces);
+        System.out.println(des.random().getValue());
+
+
+
     }
 }
