8c168e2f70eb69cffca0133226548f84625109aa
rubenhouri <ruben.houri@etu.unice.fr>
2018-10-13 20:00:22 +0200

    Ajout de la classe abstraite DiceSide , classe fille SimpleDiceSide et de l'enumeration qui comporte les ressources #5

---
+++ src/main/java/teamB/otake/Resources.java
@@ -0,0 +1,13 @@
+package main.java.teamB.otake;
+
+public enum Resources {
+
+    PG("Point de Gloire");
+
+   String resource;
+
+    Resources(String resource) {
+        this.resource=resource;
+    }
+
+}
---
+++ src/main/java/teamB/otake/SimpleDiceSide.java
@@ -0,0 +1,9 @@
+package main.java.teamB.otake;
+
+public class SimpleDiceSide extends DiceSide {
+
+    SimpleDiceSide(int valeur,Resources resources) {
+        this.valeur=valeur;
+        this.type=resources;
+    }
+}
