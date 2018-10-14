4d49c75abaeaf7b3300761ce66cf86abcc4df9da
Jason Haenlin <jasonjas5252@gmail.com>
2018-10-14 16:25:29 +0200

    #43 test pour verifier si 3 bots peuvent joeur

--- src/test/java/teamB/otake/diceForge/GameTest.java
+++ src/test/java/teamB/otake/diceForge/GameTest.java
@@ -7,10 +7,19 @@ import org.junit.Test;
 public class GameTest {
 
     @Test
-    public void testLog() {
+    public void game2BotTest() {
         Game game = new Game();
 
         String result = game.setUp(1).addBot("Rondoudou").addBot("Pichu").fire();
+
+        assertTrue(!result.isEmpty());
+    }
+
+    @Test
+    public void game3BotTest() {
+        Game game = new Game();
+
+        String result = game.setUp(1).addBot("Rondoudou").addBot("Pichu").addBot("Ronflex").fire();
 
         assertTrue(!result.isEmpty());
     }
