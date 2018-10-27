5335728a982c04fea018f3a50fc8f256afc6a4da
Jason Haenlin <jasonjas5252@gmail.com>
2018-10-27 20:16:58 +0200

    #55 supprime GameMaster et ajoute la gestion du score dans Game

--- src/main/java/fr/unice/polytech/si3/ps5/teamb/diceforge/game/Game.java
+++ src/main/java/fr/unice/polytech/si3/ps5/teamb/diceforge/game/Game.java
@@ -1,7 +1,10 @@
 package fr.unice.polytech.si3.ps5.teamb.diceforge.game;
 
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,9 +24,9 @@ public class Game extends Board {
 
     private static Logger LOGGER = LogManager.getLogger(Game.class);
 
-    private List<Bot> bots;
+    private Map<Bot, Integer> bots;
 
-    private final GameMaster gameMaster;
+    private int finalScore;
     private int round;
 
     /**
@@ -32,12 +35,11 @@ public class Game extends Board {
     public Game() {
         super();
         LOGGER.debug("init Game");
-        bots = new ArrayList<>();
-        gameMaster = new GameMaster();
+        bots = new HashMap<>();
         round = 1;
     }
 
-    public Game setUp(int round) {
+    public Game setup(int round) {
         LOGGER.info("round :" + round);
         this.round = round;
         return this;
@@ -46,20 +48,69 @@ public class Game extends Board {
     public String fire() {
         LOGGER.debug("fire !");
         for (int i = 0; i < round; i++) {
-            for (Bot bot : bots) {
+            bots.forEach((bot, score) -> {
                 bot.play(getBoardView());
+                bots.replace(bot, score + getVictoryPoint(bot.getName()));
                 LOGGER.info("Le bot " + bot.getName() + " lance les des");
                 LOGGER.info("Le bot " + bot.getName() + " a obtenue " + bot.getLastValue() + " "
                         + bot.getLastResource().toString());
-            }
+            });
         }
-        gameMaster.etablishWinner(bots);
-        return gameMaster.getWinnerMsg();
+        return etablishWinner();
     }
 
     public Game addBot(String name) {
         LOGGER.info("add bot :" + name);
-        bots.add(new Bot(name));
+        Bot bot = new Bot(name);
+        bots.put(bot, 0);
+        registrationToBoard(bot);
         return this;
+    }
+
+    private String etablishWinner() {
+        StringBuilder winnerMsg = new StringBuilder();
+        Map<String, Integer> winners = getWinnersList(new TreeMap<>(new ValueComparator(bots)));
+        if (winners.size() == 1) {
+            winners.forEach((name, score) -> {
+                winnerMsg.append("Le bot " + name + " gagne avec " + score + " points de Gloire");
+            });
+        } else {
+            winnerMsg.append("Egalite entre les joueurs ");
+            winners.forEach((name, score) -> {
+                winnerMsg.append(name + " , ");
+                finalScore = score;
+            });
+            winnerMsg.append(". Avec un total de " + finalScore);
+        }
+        return winnerMsg.toString();
+    }
+
+    private Map<String, Integer> getWinnersList(TreeMap<Bot, Integer> scoreMap) {
+        Map<String, Integer> winners = new HashMap<>();
+        winners.put(scoreMap.firstEntry().getKey().getName(), scoreMap.firstEntry().getValue());
+        int highestScore = scoreMap.pollFirstEntry().getValue();
+        scoreMap.forEach((bot, score) -> {
+            if (score == highestScore) {
+                winners.put(bot.getName(), score);
+            }
+        });
+        return winners;
+    }
+
+}
+
+class ValueComparator implements Comparator<Bot> {
+    Map<Bot, Integer> base;
+
+    public ValueComparator(Map<Bot, Integer> base) {
+        this.base = base;
+    }
+
+    public int compare(Bot a, Bot b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        }
     }
 }
