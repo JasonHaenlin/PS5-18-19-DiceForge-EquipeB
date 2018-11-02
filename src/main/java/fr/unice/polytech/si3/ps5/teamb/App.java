7ae2d2c79b28da7334dfbab6fdc9ae4bf8ebdc75
Vincent Ung <vincent.ung@etu.unice.fr>
2018-11-02 18:28:52 +0100

    Nettoyage du main

--- src/main/java/fr/unice/polytech/si3/ps5/teamb/App.java
+++ src/main/java/fr/unice/polytech/si3/ps5/teamb/App.java
@@ -4,15 +4,10 @@ import fr.unice.polytech.si3.ps5.teamb.diceforge.Engine;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import fr.unice.polytech.si3.ps5.teamb.diceforge.bot.player.Pika;
-import fr.unice.polytech.si3.ps5.teamb.diceforge.bot.player.Rem;
-import fr.unice.polytech.si3.ps5.teamb.diceforge.game.Game;
-
-import java.util.Map;
 
 public class App {
 
-    private static final boolean MAKESTATISTICS = false;
+    private static final boolean MAKESTATISTICS = true;
 
     private static Logger LOGGER = LogManager.getLogger(App.class);
     private static String result;
