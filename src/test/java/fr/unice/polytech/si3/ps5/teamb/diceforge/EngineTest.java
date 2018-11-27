8546468ac1937fa01edd8830a7fab76593533e56
Vincent Ung <vincent.ung@etu.unice.fr>
2018-11-27 16:23:12 +0100

    GH-56 réparation d'un test

--- src/test/java/fr/unice/polytech/si3/ps5/teamb/diceforge/EngineTest.java
+++ src/test/java/fr/unice/polytech/si3/ps5/teamb/diceforge/EngineTest.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import org.junit.Test;
 
 import fr.unice.polytech.si3.ps5.teamb.diceforge.bot.player.OnlyDice;
-import fr.unice.polytech.si3.ps5.teamb.diceforge.bot.player.Totoro;
+import fr.unice.polytech.si3.ps5.teamb.diceforge.bot.player.SimpleHighestExploit;
 import fr.unice.polytech.si3.ps5.teamb.diceforge.game.util.ParseHelper;
 
 /**
@@ -19,10 +19,11 @@ public class EngineTest {
 
 
     @Test
-    public void engineResultTest1() throws Exception { //Totoro is a SimpleHighestExploit strategy bot
-        String result = engine.createGame(1).addBot(OnlyDice.class).addBot(Totoro.class).fire();
-        Map<String, String> res = ParseHelper.parseResult("onlyDice", "Totoro", result);
-        System.out.println(ParseHelper.parseResult("onlyDice", "Totoro", result));
-        assertTrue(res.get("Totoro").contains("gagne 1"));
+    public void engineResultTest1() throws Exception {
+        engine.setConfFile("src/test/resources/config/moonDiceAndSunDice.json");
+        String result = engine.createGame(100).addBot(OnlyDice.class).addBot(SimpleHighestExploit.class).fire();
+        Map<String, String> res = ParseHelper.parseResult("onlyDice", "SimpleHighestExploit", result);
+        System.out.println(ParseHelper.parseResult("onlyDice", "SimpleHighestExploit", result));
+        assertTrue(res.get("SimpleHighestExploit").contains("gagne 100"));
     }
 }
